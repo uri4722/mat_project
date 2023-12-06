54c7bee7806c3e69ec5e8ab5ecaa733f5bacfdd4
Uri <uri4722@gamil.com>
2023-12-07 01:28:25 +0200

    added landing page

--- client/src/AppRoutes.jsx
+++ client/src/AppRoutes.jsx
@@ -4,12 +4,14 @@ import './App.css';
 import NoPage from './pages/NoPage';
 import ManagerRegister from './pages/ManagerRegister';
 import Home from './pages/Home';
+import LandingPage from './pages/LandingPage';
 
- function AppRoutes(props) {
+function AppRoutes(props) {
     return (
         <Routes>
 
-            <Route index element={<Home />} />
+            <Route index element={<LandingPage />} />
+            < Route path='Home' element={<Home />} />
             < Route path='ManagerRegister' element={<ManagerRegister />} />
             <Route path="*" element={<NoPage />} />
         </Routes>
---
+++ client/src/pages/LandingPage.jsx
@@ -0,0 +1,11 @@
+import BGvideo from "../compnents/BGvideo";
+
+function LandingPage() {
+    return (
+        <div>
+            <BGvideo />
+            <div className="landingPageContent"></div>
+        </div>
+    );
+}
+export default LandingPage;
