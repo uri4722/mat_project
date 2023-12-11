f457414f05e7eea196867e24e7304f6e2fa52440
uri4722 <132268605+uri4722@users.noreply.github.com>
2023-12-11 15:58:15 +0200

    joi client

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,18 +1,30 @@
 import { NavLink } from "react-router-dom"
 import Header from "../compnents/LandingPageCompnent/Header"
 import "./css/login.css"
+import { useState } from "react"
 function Login() {
+    const [user, setUser] = useState({ email: "", password: "" });
 
+    const handleChange = ({ target }) => {
+        setUser({ ...user, [target.name]: target.value });
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(user);
+
+        setUser({ name: "", password: "" });
+    }
 
 
     return (
         <>
             <Header />
             <div className="loginForm">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <h1>התחברות</h1>
-                    <input type="text" placeholder="שם מלא" />
-                    <input type="password" placeholder="סיסמא" />
+                    <input type="text" placeholder="כתובת מייל" name="email" value={user.email} onChange={handleChange} />
+                    <input type="password" placeholder="סיסמא" name="password" value={user.password} onChange={handleChange} />
                     <button>התחבר</button>
                     <NavLink to={"/managerRegister"} >יצירת חשבון</NavLink>
                 </form>
--- client/src/pages/ManagerRegister.jsx
+++ client/src/pages/ManagerRegister.jsx
@@ -1,7 +1,85 @@
-export default function ManagerRegister(props) {
-    return (
-        <div>
-            <h1>ManagerRegister</h1>
+import { NavLink } from "react-router-dom";
+import Header from "../compnents/LandingPageCompnent/Header";
+import "./css/managerRegister.css";
+import { useState } from "react";
+import Joi from 'joi';
+
+
+export default function ManagerRegister() {
+    const [user, setUser] = useState({ name: "", password: "", verifyPassword: "", email: "", phone: "" });
+    const [error, setError] = useState("");
+
+
+    const schema = Joi.object({
+        // name:
+        //     Joi.string().min(2)
+        //         .message("שדה חובה")
+        //         .required()
+        // ,
+        // password:
+        //     Joi.string()
+        //         .min(6)
+        //         .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+        //         .required()
+        // ,
+
+
+        // verifyPassword:
+        //     Joi.valid(user.password)
+        //         .required()
+        //         .messages({ 'any.only': 'הסיסמאות חייבות להיות זהות' }),
+        // email:
+        //     Joi.string()
+        //         .email({ tlds: { allow: false } })
+        //         .message("כתובת מייל לא תקינה")
+        //         .required(),
+        // phone:
+        //     Joi.string()
+        //         .pattern(new RegExp('^[0-9]{10}$'))
+        //         .message("מספר טלפון חייב להכיל 10 ספרות בלבד")
+        //         .required(),
+
+    })
+
+    const handleChange = ({ target }) => {
+        setUser({ ...user, [target.name]: target.value });
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(user);
+
+        const { error } = schema.validate(user);
+        if (error) {
+            setError(error.details[0].message);
+            return;
+        }
+        if (!error) {
+
+            setUser({ name: "", password: "", verifyPassword: "", email: "", phone: "" });
+        }
+
+
+    }
+
+    return (<>
+
+        <Header />
+        <div className="register">
+            <form onSubmit={handleSubmit}>
+                <h1>הרשמה</h1>
+                {error && <div className="error">{error}</div>}
+                <input type="text" placeholder="שם מלא" onChange={handleChange} name="name" value={user.name} />
+                <input type="text" placeholder="כתובת מייל" onChange={handleChange} name="email" value={user.email} />
+                <input type="password" placeholder="סיסמא" onChange={handleChange} name="password" value={user.password} />
+                <input type="password" placeholder="אימות סיסמא" onChange={handleChange} name="verifyPassword" value={user.verifyPassword} />
+                <input type="text" placeholder="טלפון" onChange={handleChange} name="phone" value={user.phone} />
+                <button>הרשם</button>
+
+                <NavLink to={"/login"} >התחברות </NavLink>
+
+            </form>
         </div>
+    </>
     );
 }
