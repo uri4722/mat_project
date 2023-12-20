a22ede30440163f7c53ed82873716304a00eb02e
Uri <uri4722@gamil.com>
2023-12-21 00:06:34 +0200

    fill sql commitments

---
+++ sql/fill_commitments_table.sql
@@ -0,0 +1,16 @@
+INSERT INTO commitments (maschet, user_email, passed_away_id, start_date)
+VALUES
+  ('Berachot', 'user1@example.com', 1, '2023-01-01' ),
+  ('Shabbat', 'user2@example.com', 1, '2023-02-15' ), 
+  ('Eruvin', 'user3@example.com', 1, '2023-03-01' ),
+  ('Pesachim', 'user5@example.com', 1, '2023-04-15'),
+  ('Yoma', 'user5@example.com', 1, '2023-05-01'),
+  ('Yoma', 'user5@example.com', 2, '2023-05-01'),
+  ('Sukkah', 'user1@example.com', 2, '2023-06-01'),
+  ('Beitzah', 'user2@example.com', 1, '2023-07-15'),
+  ('Rosh Hashana', 'user3@example.com', 1, '2023-08-01'),
+  ('Taanit', 'user4@example.com', 1, '2023-09-01'),
+    ('Taanit', 'user5@example.com', 2, '2023-05-01'),
+  ('Megillah', 'user5@example.com', 1, '2023-10-15');
+
+
