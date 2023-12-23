1317aa5fd81bc7b0ae0844da7476204f4e4ab1af
Uri <uri4722@gamil.com>
2023-12-23 23:33:32 +0200

    fix sql

---
+++ sql/fill_users_table.sql
@@ -0,0 +1,13 @@
+INSERT INTO `users` (`name`, `email`, `password`)
+VALUES 
+('אורי שניידר', 'uri4722@gmail.com', 'uri12345'),
+('מנחם מנדל שטרן', 'mendy@example.com', 'abc123'),
+ ('יוסף ישראל כהן', 'yossi@example.com', '123yossi'),   
+ ('שמואל דוד לוי', 'shmuel@example.com', 'le123456'),
+ ('אליהו חיים וינברג', 'eliyahu@example.com', 'cha987'),
+ ('שלמה זלמן פרידמן', 'shalom@example.com', 'fried123'),
+ ('יצחק אייזיק ברקוביץ', 'yitzhak@example.com', 'bar456'),  
+ ('מאיר מרדכי שapiro', 'meir@example.com', 'shap321'),
+ ('מנשה אליעזר וכטפהנדלר', 'menashe@example.com', 'vax987'),
+ ('נפתלי הירש הורוביץ', 'naftali@example.com', 'naf123'),
+ ('אברהם יצחק רוזנברג', 'avi@example.com', 'rosen456');
