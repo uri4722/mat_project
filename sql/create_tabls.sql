1e1e3dd22008867eb6e26578af409ae310b31c60
Uri <uri4722@gamil.com>
2023-12-23 22:44:25 +0200

    Update create_tabls.sql

--- sql/create_tabls.sql
+++ sql/create_tabls.sql
@@ -41,7 +41,7 @@ CREATE TABLE `users`(
     `user_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(255) NOT NULL,
     `email` VARCHAR(255) NOT NULL,
-    `password` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL
 );
 
 ALTER TABLE
