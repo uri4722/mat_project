64e08230e2c1e0ad1e61454920728c5d3e03bfbd
Uri <uri4722@gamil.com>
2023-12-23 23:14:47 +0200

    Update create_tabls.sql

--- sql/create_tabls.sql
+++ sql/create_tabls.sql
@@ -1,15 +1,9 @@
 USE mat_project;
 
-CREATE TABLE `storys`(
-    `story_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `user_id` INT NOT NULL,
-    `passed_away_id` INT NOT NULL,
-    `title` VARCHAR(255) NOT NULL,
-    `story` VARCHAR(255) NOT NULL
-);
+
 CREATE TABLE `passed_away`(
     `passed_away_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `manager_id` INT NOT NULL,
+    `manager_id` INT UNSIGNED NOT NULL,
     `name` VARCHAR(255) NOT NULL,
     `year_death` INT NOT NULL,
     `month_death` INT NOT NULL,
@@ -21,6 +15,7 @@ CREATE TABLE `passed_away`(
     `rabbi` TINYINT(1) NULL DEFAULT 0,
     `age` INT NULL
 );
+
 CREATE TABLE `managers`(
     `manager_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `full_name` VARCHAR(255) NOT NULL,
@@ -28,15 +23,27 @@ CREATE TABLE `managers`(
     `password` VARCHAR(255) NOT NULL,
     `phone` VARCHAR(255) NULL
 );
+
+CREATE TABLE `storys`(
+    `story_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `user_id` INT UNSIGNED NOT NULL,
+    `passed_away_id` INT UNSIGNED NOT NULL,
+    `title` VARCHAR(255) NOT NULL,
+    `story` VARCHAR(255) NOT NULL
+);
+
+
 CREATE TABLE `commitments`(
     `commitment_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `maschet` VARCHAR(255) NOT NULL,
     `round_number` INT NOT NULL DEFAULT 1,
-    `user_id` INT NOT NULL,
-    `passed_away_id` INT NOT NULL,
+    `user_id` INT UNSIGNED NOT NULL,
+    `passed_away_id` INT UNSIGNED NOT NULL,
     `start_date` DATE NOT NULL,
     `deadline` DATE NULL
+
 );
+
 CREATE TABLE `users`(
     `user_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(255) NOT NULL,
@@ -44,13 +51,18 @@ CREATE TABLE `users`(
     `password` VARCHAR(255) NOT NULL
 );
 
-ALTER TABLE
-    `passed_away` ADD CONSTRAINT `passed_away_manager_id_foreign` FOREIGN KEY(`manager_id`) REFERENCES `managers`(`manager_id`);
-ALTER TABLE
-    `commitments` ADD CONSTRAINT `commitments_pass_away_id_foreign` FOREIGN KEY(`pass_away_id`) REFERENCES `passes_away`(`pass_away_id`);
-ALTER TABLE
-    `storys` ADD CONSTRAINT `storys_story_id_foreign` FOREIGN KEY(`story_id`) REFERENCES `users`(`user_id`);
-ALTER TABLE
-    `commitments` ADD CONSTRAINT `commitments_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `users`(`user_id`);
-ALTER TABLE
-    `storys` ADD CONSTRAINT `storys_pass_away_id_foreign` FOREIGN KEY(`pass_away_id`) REFERENCES `passes_away`(`pass_away_id`);
+-- here foreign keys
+ALTER TABLE `passed_away`
+ADD FOREIGN KEY (`manager_id`) REFERENCES `managers`(`manager_id`);
+
+ALTER TABLE `storys`
+ADD FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`),
+ADD FOREIGN KEY (`passed_away_id`) REFERENCES `passed_away`(`passed_away_id`);
+
+
+ALTER TABLE `commitments`
+ADD FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`),
+ADD FOREIGN KEY (`passed_away_id`) REFERENCES `passed_away`(`passed_away_id`);
+
+
+
