c18621dcb05d7d67180b698a866c53c42cd7a02f
uri4722 <132268605+uri4722@users.noreply.github.com>
2024-10-28 14:58:23 +0200

    Made password encrypted

--- sql/fill_users_table.sql
+++ sql/fill_users_table.sql
@@ -1,8 +1,10 @@
 INSERT INTO `users` (`name`, `email`, `password`, `role`, `user_id`)
 VALUES 
-('אורי שניידר', 'uri4722@gmail.com', 'uri12345', 'manager', 1),
+('אורי שניידר', 'uri4722@gmail.com', 'Y6sw3/bE6AL3kj9/U+6MBpXP602mPWEREocFglT/f/U=.Uxe/mNyDCixa4CxiIgppY+K8OalIdrct4Lw2h55gJBY=', 'manager', 1),
+-- password uri12345
 ('מנחם מנדל שטרן', 'mendy@example.com', 'abc123', DEFAULT, DEFAULT),
-('יוסף ישראל כהן', 'yossi@example.com', '123yossi', DEFAULT, DEFAULT),   
+('יוסף ישראל כהן', 'yossi@example.com', 'QpO7NKy8pyuzzkG+VJMsYMWTaMdpvVHdeWGAYlo+2Xk=.ZNabnayq/UijL1ks6P2m3IOMVfw7sRrd+4TuH9pTEhM=', DEFAULT, DEFAULT), 
+-- password 123yossi  
 ('שמואל דוד לוי', 'shmuel@example.com', 'le123456', DEFAULT, DEFAULT),
 ('אליהו חיים וינברג', 'eliyahu@example.com', 'cha987', DEFAULT, DEFAULT),
 ('שלמה זלמן פרידמן', 'shalom@example.com', 'fried123', DEFAULT, DEFAULT),
@@ -11,5 +13,5 @@ VALUES
 ('מנשה אליעזר וכטפהנדלר', 'menashe@example.com', 'vax987', DEFAULT, DEFAULT),
 ('נפתלי הירש הורוביץ', 'naftali@example.com', 'naf123', DEFAULT, DEFAULT),
 ('אברהם יצחק רוזנברג', 'avi@example.com', 'rosen456', DEFAULT, DEFAULT),
-('מנהל', 'm@m.com', 'qwe123', 'manager', 20);
--- -- סיסמא qwe123
+('מנהל', 'm@m.com', '15Eo3vE+0yz70i2ZVHHcNL31rrRoH862HFDwf06Nq24=.inVVB1TEEvfh79T0ZZ1Dv87glI19y5WS6c0HM/znAKE=', 'manager', 20);
+-- -- password qwe123
